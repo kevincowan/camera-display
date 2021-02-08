4b70d7c19043084db61e66814842c5a35b0f9eee
Robert McNeill <rmcneill@shopkeep.com>
2021-02-08 16:38:35 +0000

    Adding CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-      contentBase: './dist',
-    },
-  };
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
